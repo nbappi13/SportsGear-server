8dae6226b6a5e39332d169981881f99a128ee37c
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2025-01-28 00:37:02 +0600

    applied CRUD operation post and get to use database using mongodb atlas

--- index.js
+++ index.js
@@ -9,13 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0fter.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0fter.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,24 +22,36 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const equipCollection = client.db("equipmentstDB").collection('equipments');
+
+    app.post('/addEquip', async (req, res) => {
+      const newAddEquip = req.body;
+      console.log(newAddEquip);
+      const result = await equipCollection.insertOne(newAddEquip);
+      res.send(result);
+    });
+
+    app.get('/equipment', async (req, res) => {
+      const equipment = await equipCollection.find().limit(6).toArray(); // Limit to 6 items
+      res.send(equipment);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    // await client close;
   }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("b10 server is runnnig!");
+  res.send("b10 server is running!");
 });
 
 app.listen(port, () => {
-  console.log(`b10 server is runnig on port: ${port}`);
+  console.log(`b10 server is running on port: ${port}`);
 });
